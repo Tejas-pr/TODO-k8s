ecf81ffc13b00f3369a84d6148102d1ec7ba1c14
Tejas <tejas.teju02@gmail.com>
2026-01-02 23:15:10 +0530

    basic application

---
+++ backend/index.ts
@@ -0,0 +1,22 @@
+import express from "express"
+
+const app = express();
+
+app.use(express.json());
+
+const BACKEND_PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "IM up!!",
+        db: DATABASE_URL
+    });
+});
+
+console.log("backend port is : ", BACKEND_PORT);
+console.log("database port is : ", DATABASE_URL);
+
+app.listen(BACKEND_PORT, () => {
+    console.log(`app running in the port: ${BACKEND_PORT}`)
+});
